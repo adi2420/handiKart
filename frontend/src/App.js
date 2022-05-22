a79d85242faa0d0c8e75dc8660b5ca07e0c1f711
Aditya Bharadwaj <bhardwajaditya31@gmail.com>
2022-05-22 17:33:11 +0530

    created react app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">HandiCart</a>
+      </header>
+      <main>list Products</main>
+    </div>
+  );
+}
+
+export default App;
